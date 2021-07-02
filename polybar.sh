3d188bfc27435d8bea68d379fb06becf09ae57e3
Geoff Wagner <wageof@gmail.com>
2021-07-01 22:02:56 -0500

    Get resolution

--- polybar.sh
+++ polybar.sh
@@ -5,11 +5,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --log=info -r main &> ~/.polybar.logs 2>&1 &
-  done
-else
-  polybar --log=info -r main &> ~/.polybar.logs 2>&1 &
-fi
+MONS="$(polybar -m)"
+S="$(echo $MONS | cut -f2 -d ' ')"
+M="$(echo $MONS | cut -f1 -d ':')"
+W="$(echo $MONS | cut -f2 -d ' ' | cut -f1 -d '+' | cut -f1 -d 'x')"
+W="$(expr $W - 8)"
+H="$(echo $MONS | cut -f1 -d '+' | cut -f2 -d 'x')"
+
+
+MONITOR=$M RESOLUTION=$S WIDTH=$W HEIGHT=$H polybar --log=info -r main &> ~/.polybar.logs 2>&1 &
 
