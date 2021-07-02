4a4ad6d2f9bc19ceeb8cceff95fc82ff62d4eb2e
Geoff Wagner <wageof@gmail.com>
2021-07-01 21:38:00 -0500

    Detect the monitor with polybar script

--- polybar.sh
+++ polybar.sh
@@ -5,4 +5,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar --log=info -r main &> ~/.polybar.logs 2>&1 &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --log=info -r main &> ~/.polybar.logs 2>&1 &
+  done
+else
+  polybar --log=info -r main &> ~/.polybar.logs 2>&1 &
+fi
+
