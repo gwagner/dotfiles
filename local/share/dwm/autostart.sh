a767b146a009f11386ae609fd4f52e32479c968c
Geoff Wagner <wageof@gmail.com>
2021-07-08 21:42:03 -0500

    Minor config updates

--- local/share/dwm/autostart.sh
+++ local/share/dwm/autostart.sh
@@ -4,29 +4,32 @@ killall -q nitroen
 killall -q picom
 killall -q dunst
 killall -q firefox
+killall -q obs
 killall -q teams
 killall -q slack
 killall -q numlockx
 killall -q solaar
 
 startApp() {
+    CMD="$(echo $1 | cut -f1 -d ' ' )"
     if pgrep -x "$1" >/dev/null
     then
-        echo "$1 is running"
+        echo "$CMD is running"
     else
         $1 &
-        echo "$1 is starting"
+        echo "$CMD is starting"
     fi
 }
 
 
 sleep 1
 
-startApp "/usr/bin/nitrogen --restore"
+startApp "nitrogen --restore"
 startApp picom
 startApp dunst
+startApp "obs --startvirtualcam"
 startApp firefox
 startApp teams
 startApp slack
 startApp numlockx
-startApp solaar -w hide
+startApp "solaar -w hide"
