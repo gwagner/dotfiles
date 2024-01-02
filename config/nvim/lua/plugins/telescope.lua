72ea227509f12122e4253b524cb1f2d755988dee
Geoff Wagner <wageof@gmail.com>
2024-01-01 23:19:31 -0600

    Add live grep support

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,15 @@
 return {
   "nvim-telescope/telescope-file-browser.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-  keys = { 
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "BurntSushi/ripgrep",
+  },
+  keys = {
     { "<leader>fo", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", desc = "Open File Browser" },
-    { "<leader>ff", ":Telescope find_files select_buffer=true<CR>", desc = "Find Files" }
+    { "<leader>ff", ":Telescope find_files select_buffer=true<CR>", desc = "Find Files" },
+    { "<leader>fg", ":Telescope live_grep select_buffer=true<CR>", desc = "Find Files" },
   }
 }
 
