e92e15ec219b4c490e5b4f2c0add3782538a9a22
Geoff Wagner <wageof@gmail.com>
2024-01-01 22:01:42 -0600

    Update Sources

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -1,14 +1,17 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    keys = {
-      { "<leader>t", ":Neotree toggle filesystem reveal left dir=%:p:h git_base=HEAD<CR>", desc="Neotree Open"},
-      { "<leader>u", ":Neotree dir=../<CR>", desc="Neotree up one directory" },
-    }
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  opts = {
+    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+  },
+  keys = {
+    { "<leader>t", ":Neotree toggle filesystem reveal left dir=%:p:h git_base=HEAD<CR>", desc = "Neotree Open" },
+    { "<leader>u", ":Neotree dir=../<CR>",                                               desc = "Neotree up one directory" },
+  }
 }
