5e3cbac022ccdc9feeb054a12d7580c7decdab4f
Geoff Wagner <wageof@gmail.com>
2024-01-02 20:14:59 -0600

    Adjust some params

--- config/nvim/lua/plugins/trouble.lua
+++ config/nvim/lua/plugins/trouble.lua
@@ -2,7 +2,8 @@ return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
-    height = 5,
+    height = 8,
+    padding = false,
   },
   keys = {
     { "<leader>tt", ":TroubleToggle<CR>", desc = "Open Trouble" },
