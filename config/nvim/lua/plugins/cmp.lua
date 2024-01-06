7195d4612df735fa2203257b7501f3909c98d2f0
Geoff Wagner <wageof@gmail.com>
2024-01-05 21:02:07 -0600

    stop default select

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -15,6 +15,7 @@ return {
   config = function()
     local cmp = require("cmp")
     cmp.setup({
+      preselect = cmp.PreselectMode.None,
       snippet = {
         expand = function(args)
           require('luasnip').lsp_expand(args.body)
