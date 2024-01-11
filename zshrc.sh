b039d5540118eb2b8adcd8053452188f7625227e
Geoff Wagner <wageof@gmail.com>
2024-01-10 21:08:10 -0600

    Give the primary session a name for ssh purposes

--- zshrc.sh
+++ zshrc.sh
@@ -166,7 +166,7 @@ fi
 
 # Startup TMUX
 if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-  tmux attach || tmux new-session
+  tmux attach -t "primary" || tmux new-session -t "primary"
 fi
 
 fastfetch
