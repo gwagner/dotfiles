e4d8166ae188bfc62f3cffb8890e8551a3bee5d2
Geoff Wagner <wageof@gmail.com>
2024-01-12 21:03:02 -0600

    setup global clipboard

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -69,3 +69,6 @@ vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGai
   command = "if mode() != 'c' | checktime | endif",
   pattern = { "*" },
 })
+
+-- always use the system clipboard
+vim.opt.clipboard="unnamedplus"
