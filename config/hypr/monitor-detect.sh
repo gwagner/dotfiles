35e36233254c300759dde4fcedbc5a017089fce6
Geoff Wagner <wageof@gmail.com>
2024-03-10 22:27:04 -0500

    more configs for hyprland to make it work between computers

--- config/hypr/monitor-detect.sh
+++ config/hypr/monitor-detect.sh
@@ -2,5 +2,7 @@
 
 if [ "$HOSTNAME" = framework ]; then
    hyprctl keyword monitor eDP-1,2256x1504@60,0x0,1.566667  
+else
+  hyprctl keyword monitor HDMI-A-1,3440x1440,auto,1
 fi
 
---
+++ config/hypr/waybar-detect.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$HOSTNAME" = framework ]; then
+  waybar -c ~/.config/waybar/config-framework -s ~/.config/waybar/style-framework.css 
+else
+  waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css
+fi
+
