97904890277c8f45b0ac6bac3f80d2b59a463454
Geoff Wagner <wageof@gmail.com>
2024-03-11 21:45:23 -0500

    Updates for usability

--- config/hypr/monitor-detect.sh
+++ config/hypr/monitor-detect.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ "$HOSTNAME" = framework ]; then
-   hyprctl keyword monitor eDP-1,2256x1504@60,0x0,1.566667  
+  hyprctl keyword monitor eDP-1,2256x1504@60,0x0,1.566667  
+  hyprctl --batch "dispatch workspace 2; dispatch layoutmsg orientationleft; dispatch workspace 1; dispatch layoutmsg orientationleft; dispatch layoutmsg mfact .7"
 else
   hyprctl keyword monitor HDMI-A-1,3440x1440,auto,1
 fi
