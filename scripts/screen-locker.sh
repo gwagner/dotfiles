e3d8fc920733b44e82876fbcfb5f8bd64b71784d
Geoff Wagner <wageof@gmail.com>
2024-03-12 21:34:14 -0500

    Fix screen locker and monitor detect

--- scripts/screen-locker.sh
+++ scripts/screen-locker.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+DISPLAY_MANAGER=`loginctl show-session $(loginctl show-user $(whoami) -p Display --value) -p Type --value`
+
+if [[ "$DISPLAY_MANAGER" == "wayland" ]]; then
   swaylock -f -C ~/.config/swaylock/config
 else
   /usr/bin/i3lock -i /home/gwagner/backgrounds/lock-screen-background.jpg -F --ring-color=3b4252 --inside-color=2e3440 --insidever-color=2e3440 --ringver-color=2e3440 --insidewrong-color=2e3440 --ringwrong-color=2e3440 --keyhl-color=4c566a --bshl-color=4c566a --verif-color=FFFFFF --wrong-color=bf616a --modif-color=ebcb8b --noinput-text=""
