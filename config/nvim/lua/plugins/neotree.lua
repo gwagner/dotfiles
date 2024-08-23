65d8d4f1bb49ac8ea0fab079bde14f94c7d9d3bf
Geoff Wagner <wageof@gmail.com>
2024-08-23 15:12:45 -0500

    Update neotree to show hiden files

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -29,6 +29,20 @@ return {
   config = function()
     require("neo-tree").setup({
       close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            '.git',
+            '.github',
+            '.DS_Store',
+            'thumbs.db'
+          },
+          never_show = {},
+        }
+      },
     })
   end
 }
