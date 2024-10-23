8e475cc9c905b09e71947a404483a25899013189
Geoff Wagner <wageof@gmail.com>
2024-10-23 09:11:00 -0500

    Add keybind for neotree

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -7,6 +7,9 @@ return {
     "MunifTanjim/nui.nvim",
     "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  keys = {
+    { "<leader>nt", ":Neotree toggle<CR>", desc = "Toggle Neotree" },
+  },
   config = function()
     require("neo-tree").setup({
       close_if_last_window = true,
