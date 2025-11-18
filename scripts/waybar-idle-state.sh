ca980f5c2989881c72cfd1a9befc0e1f5353229f
Geoff Wagner <wageof@gmail.com>
2025-11-17 21:30:44 -0600

    fix bad pluralization

--- scripts/waybar-idle-state.sh
+++ scripts/waybar-idle-state.sh
@@ -4,10 +4,17 @@ TEXT="󰒲 "
 TOOLTIP="No clients inhibiting idle"
 if hyprctl clients | grep -q "Idle: 1"; then
 
+  COUNT=$(hyprctl clients | grep "Idle: 1" | wc -l)
+  LABEL="clients"
+
+  if [ "$COUNT" -eq 1 ]; then
+    LABEL="client"
+  fi
+
   CLIENTS="`hyprctl clients -j | jq -r '[.[] | select(.inhibitingIdle == true) | .title] | join(" and ")'`"
 
   TEXT="󱎴 "
-  TOOLTIP="`hyprctl clients | grep "Idle: 1" | wc -l` clients inhibiting idle\n$CLIENTS"
+  TOOLTIP="$COUNT $LABEL inhibiting idle\n$CLIENTS"
 fi
 
 
