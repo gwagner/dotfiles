8541ca00b70c9d44433ceda71a54b943ef3f6a2a
Geoff Wagner <wageof@gmail.com>
2026-07-10 15:54:15 -0500

    Updates to hyprland

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,47 @@
+-- return {
+-- 'nvim-treesitter/nvim-treesitter',
+-- dependencies = {
+--    'nvim-treesitter/nvim-treesitter-context',
+--    "OXY2DEV/markview.nvim",
+--  },
+--  branch = "main",
+--  opts = {
+--      folds = {
+--        enable = true,
+--      },
+--      auto_install = true,
+--      sync_install = false,
+--      ignore_install = {},
+--      ensure_installed = {
+--        "go", "gomod", "gowork", "gotmpl", "css", "html", "javascript", "typescript", "jsdoc", "json", "c", "java",
+--        "toml", "tsx",
+--        "lua", "cpp", "python", "rust", "jsonc", "dart", "css", "yaml", "vue", "markdown", "markdown_inline", "php",
+--        "php_only", "latex", "typst", "zig", "sql", "comment"
+--      },
+--      highlight = {
+--        enable = true,
+--      }
+--  },
+--  config = function()
+
+--    local context = require("treesitter-context");
+--    context.setup({
+--      enabled = true,
+--    });
+--  end
+--}
+
 return {
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-context',
-    "OXY2DEV/markview.nvim",
-  },
+  "romus204/tree-sitter-manager.nvim",
+  dependencies = {}, -- tree-sitter CLI must be installed system-wide
   config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      folds = {
-        enable = true,
-      },
-      auto_install = true,
-      sync_install = false,
-      ignore_install = {},
+    require("tree-sitter-manager").setup({
       ensure_installed = {
         "go", "gomod", "gowork", "gotmpl", "css", "html", "javascript", "typescript", "jsdoc", "json", "c", "java",
         "toml", "tsx",
         "lua", "cpp", "python", "rust", "jsonc", "dart", "css", "yaml", "vue", "markdown", "markdown_inline", "php",
         "php_only", "latex", "typst", "zig", "sql", "comment"
       },
-      highlight = {
-        enable = true,
-      }
     })
-
-    local context = require("treesitter-context");
-    context.setup({
-      enabled = true,
-    });
-  end
+  end,
 }
