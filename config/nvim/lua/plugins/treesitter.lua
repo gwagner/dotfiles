b43300d699e32eb14a1401031bdcd5d55c3333e4
Geoff Wagner <wageof@gmail.com>
2026-08-15 14:28:55 -0500

    Update treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,47 +1,44 @@
--- return {
--- 'nvim-treesitter/nvim-treesitter',
--- dependencies = {
---    'nvim-treesitter/nvim-treesitter-context',
---    "OXY2DEV/markview.nvim",
---  },
---  branch = "main",
---  opts = {
---      folds = {
---        enable = true,
---      },
---      auto_install = true,
---      sync_install = false,
---      ignore_install = {},
---      ensure_installed = {
---        "go", "gomod", "gowork", "gotmpl", "css", "html", "javascript", "typescript", "jsdoc", "json", "c", "java",
---        "toml", "tsx",
---        "lua", "cpp", "python", "rust", "jsonc", "dart", "css", "yaml", "vue", "markdown", "markdown_inline", "php",
---        "php_only", "latex", "typst", "zig", "sql", "comment"
---      },
---      highlight = {
---        enable = true,
---      }
---  },
---  config = function()
-
---    local context = require("treesitter-context");
---    context.setup({
---      enabled = true,
---    });
---  end
---}
-
 return {
-  "romus204/tree-sitter-manager.nvim",
+  'nvim-treesitter/nvim-treesitter',
+  branch = "main",
+  build = ':TSUpdate',
   dependencies = {}, -- tree-sitter CLI must be installed system-wide
   config = function()
-    require("tree-sitter-manager").setup({
-      ensure_installed = {
-        "go", "gomod", "gowork", "gotmpl", "css", "html", "javascript", "typescript", "jsdoc", "json", "c", "java",
-        "toml", "tsx",
-        "lua", "cpp", "python", "rust", "jsonc", "dart", "css", "yaml", "vue", "markdown", "markdown_inline", "php",
-        "php_only", "latex", "typst", "zig", "sql", "comment"
-      },
+    local treesitter = require("nvim-treesitter")
+    treesitter.setup()
+    treesitter.install({
+      "go",
+      "gomod",
+      "gowork",
+      "gotmpl",
+      "css",
+      "html",
+      "javascript",
+      "typescript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "c",
+      "java",
+      "toml",
+      "tsx",
+      "lua",
+      "cpp",
+      "python",
+      "rust",
+      "dart",
+      "css",
+      "yaml",
+      "vue",
+      "markdown",
+      "markdown_inline",
+      "php",
+      "php_only",
+      "latex",
+      "typst",
+      "zig",
+      "sql",
+      "comment",
     })
   end,
 }
