8098ac8da981e52964dd592a922e1f72cfa4eda1
Geoff Wagner <wageof@gmail.com>
2026-09-02 21:52:45 -0500

    fix backgrounds

--- config/hypr/laptop/hyprland.lua
+++ config/hypr/laptop/hyprland.lua
@@ -83,6 +83,7 @@ hl.config({
     mfact = 0.5,
   },
   misc = {
+    disable_splash_rendering = true,
     disable_hyprland_logo = true,
     mouse_move_enables_dpms = true,
     key_press_enables_dpms = false,
